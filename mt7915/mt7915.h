784ff6906ae9422a076dfa5258da31d81e6559cf
Felix Fietkau <nbd@nbd.name>
2021-04-29 15:28:45 +0200

    mt76: mt7915: remove irq parameter from mt7915_mmio_init
    It is unused
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7915/mt7915.h
+++ mt7915/mt7915.h
@@ -488,7 +488,8 @@ void mt7915_mac_sta_remove(struct mt76_dev *mdev, struct ieee80211_vif *vif,
 void mt7915_mac_work(struct work_struct *work);
 void mt7915_mac_reset_work(struct work_struct *work);
 void mt7915_mac_sta_rc_work(struct work_struct *work);
-int mt7915_mmio_init(struct mt76_dev *mdev, void __iomem *mem_base, int irq);
+void mt7915_mac_update_stats(struct mt7915_phy *phy);
+int mt7915_mmio_init(struct mt76_dev *mdev, void __iomem *mem_base);
 void mt7915_mac_twt_teardown_flow(struct mt7915_dev *dev,
 				  struct mt7915_sta *msta,
 				  u8 flowid);
