1df74759c5e5c668203d7c204b551fdfc7b3f5b5
Lorenzo Bianconi <lorenzo@kernel.org>
2025-09-28 18:01:37 +0200

    wifi: mt76: mt7996: Add CSA support for MLO client
    Introduce Channel-Switch support for STA MLO interface mode.
    
    Fixes: f5160304d57c ("wifi: mt76: mt7996: Enable MLO support for client interfaces")
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mt7996/mt7996.h
+++ mt7996/mt7996.h
@@ -251,11 +251,15 @@ struct mt7996_vif_link {
 	struct mt76_vif_link mt76; /* must be first */
 
 	struct mt7996_sta_link msta_link;
+	struct mt7996_vif *vif;
 	struct mt7996_phy *phy;
 
 	struct cfg80211_bitrate_mask bitrate_mask;
 
 	u8 mld_idx;
+
+	struct work_struct csa_work;
+	struct timer_list csa_timer;
 };
 
 struct mt7996_vif_link_info {
